9e365cc7784d6aa9e0ffd15c7859cec5c2307fb7
Nathanael Iung <nathanaeliung@hotmail.com>
2026-01-28 15:41:51 -0300

    - Innitial commit

---
+++ src/main/java/com/protocolo360/api/Protocolo360ApiApplication.java
@@ -0,0 +1,13 @@
+package com.protocolo360.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Protocolo360ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Protocolo360ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/protocolo360/api/Protocolo360ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.protocolo360.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Protocolo360ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
