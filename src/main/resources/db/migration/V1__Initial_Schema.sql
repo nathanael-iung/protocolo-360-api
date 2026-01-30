ec308e0aad937aa450be1323c5fd2363f0a11b04
Nathanael Iung <nathanaeliung@hotmail.com>
2026-01-30 12:13:01 -0300

    - Setting up neon postgress db connection

---
+++ src/main/resources/db/migration/V1__Initial_Schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    birth_date DATE,
+    gender VARCHAR(50),
+    phone VARCHAR(20),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
